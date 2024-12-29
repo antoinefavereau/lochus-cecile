8ccf0cf89413788ecb4ddfd59edc254b89f02101
Antoine Favereau <antoinefavereau45@gmail.com>
2024-12-29 12:15:13 +0100

    refactor projects section layout and improve marquee link styling

--- components/layout/home/projects.tsx
+++ components/layout/home/projects.tsx
@@ -37,7 +37,7 @@ export default function Projects() {
   const [selectedProject, setSelectedProject] = useState<Project>(projects[0]);
 
   return (
-    <section className="flex flex-col gap-8">
+    <section className="flex flex-col">
       <div className="relative flex flex-col items-center gap-32 px-8 py-40 min-h-screen">
         {projects.map((project) => (
           <div key={project.id} className="absolute inset-0">
@@ -65,10 +65,12 @@ export default function Projects() {
           </Link>
         ))}
       </div>
-      <Link className="group flex overflow-hidden" href="/projets">
-        {/* This is a marquee so we need to duplicate this text at least 20 times with a js loop */}
+      <Link className="group flex py-8 overflow-hidden" href="/projets">
         {new Array(20).fill(0).map((_, index) => (
-          <div key={index} className="shrink-0 flex items-center gap-4 pe-16 text-4xl animate-[horizontalMarquee_3s_linear_infinite]">
+          <div
+            key={index}
+            className="shrink-0 flex items-center gap-4 pe-16 text-4xl animate-[horizontalMarquee_3s_linear_infinite]"
+          >
             <span>Voir plus de projets</span>
             <svg
               className="text-primary rotate-[-30deg] group-hover:rotate-0 transition-rotate duration-300 ease-in-out"
