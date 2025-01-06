a7201160e9c0f0938c68444ac2706b6d7e91d91b
Antoine Favereau <antoinefavereau45@gmail.com>
2025-01-06 10:04:05 +0100

    fix: adjust gap and padding in projects section for improved layout

--- components/layout/home/projects.tsx
+++ components/layout/home/projects.tsx
@@ -43,7 +43,7 @@ export default function Projects() {
 
   return (
     <section className="flex flex-col gap-8 py-16">
-      <div className="relative flex flex-col items-center gap-32 px-8 pt-64 pb-80 min-h-screen">
+      <div className="relative flex flex-col items-center gap-32 md:gap-40 px-8 py-[50vh] min-h-screen">
         {projects.map((project) => (
           <div key={project.id} className="absolute inset-0">
             <Image
