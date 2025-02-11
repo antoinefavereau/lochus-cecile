b630cb0e1f3b547fdd635f8bffd53d35ea54d029
Antoine Favereau <93597506+antoinefavereau@users.noreply.github.com>
2025-02-11 16:35:54 +0100

    increase upload size limit

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default () => ({
+    upload: {
+        config: {
+            providerOptions: {
+                localServer: {
+                    maxFileSize: 400 * 1024 * 1024,
+                },
+            },
+        },
+    },
+});
